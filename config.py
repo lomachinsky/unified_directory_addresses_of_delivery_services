8fd06a4a9884fe962da80b73d6b598d4d6ad31da
Admin <v.lomachinsky@gmail.com>
2022-03-25 14:29:17 +0300

    remove file

--- config.py
+++ config.py
@@ -6,3 +6,11 @@ database_settings = {
     'password': '82371'
 }
 print_status = 1
+settings_novaposhta = {
+    "key": "f711f674afa103c78970c025d46ef8ed",
+    "url": "https://api.novaposhta.ua/v2.0/json/"
+}
+settings_ukrposhta = {
+    "key": "38dfd023-2571-339b-81da-ef16b13ec736",
+    "url": "https://www.ukrposhta.ua/address-classifier-ws/"
+}
